8279bc7ffc9f31cd9ffb763baa9fd6ce782b243b
Zantezu <danilo.patrucco@gmail.com>
2019-06-16 15:27:00 -0400

    Task: static created and a test echo called to verify that no error show up

--- Public/index.php
+++ Public/index.php
@@ -1,3 +1,37 @@
 <?php
-echo 'test';
+/** program to output html table from a csv file */
+main::start();
+
+/** main class that will run when opening the index page */
+class main {
+
+    static public function start() {
+        $records = csv::getRecords();
+        $page = html::generateTable($records);
+       /** $table = 'test'; */
+        system::printPage($table);
+
+    }
+}
+
+class csv {
+
+    static public function getRecords() {}
+
+}
+
+class html {
+
+    static public function generateTable() {}
+}
+
+class system {
+
+    static public function printPage($page) {
+
+        echo $page;
+    }
+}
+
+
 
