8faab688cd0129f6b352f9eb185ad5fb99ae158c
Zantezu <danilo.patrucco@gmail.com>
2019-06-16 22:45:02 -0400

    Task : testing the csv to array conversion class (WIP_1)

--- Public/index.php
+++ Public/index.php
@@ -1,21 +1,48 @@
 <?php
 /** program to output html table from a csv file */
-main::start();
+
+main::start("database.csv");
 
 /** main class that will run when opening the index page */
+
 class main {
 
-    static public function start() {
-        $records = csv::getRecords();
-        $table = html::generateTable($records);
-        system::printPage($table);
+        static public function start($filename) {
 
-    }
+            $records = csv::getRecords($filename);
+            $table = html ::generateTable($records);
+            system::printPage($table);
+            print_r($records);
+        }
+/**     $records = csv::getRecords();
+        $table = html::generateTable($records);
+
+*/
 }
+
+/** remember to refactor!!!!!!! */
 
 class csv {
 
-    static public function getRecords() {}
+    static public function getRecords($filename) {
+
+/** Open the file for reading */
+        if (($csv_file = fopen("{$filename}", "r")) !== FALSE)
+        {
+            /** Each line in the file is converted into an individual array that we call $data
+            The items of the array are comma separated */
+            while (($data = fgetcsv($csv_file, 1000, ",")) !== FALSE)
+            {
+                /** Each individual array is being pushed into the nested array */
+                $records [] = $data;
+            }
+
+            /** Close the file */
+            fclose($csv_file);
+            return $records;
+        }
+
+    }
 
 }
 
@@ -24,7 +51,7 @@ class html {
     static public function generateTable() {}
 }
 
-class system {
+final class system {
 
     static public function printPage($page) {
 
